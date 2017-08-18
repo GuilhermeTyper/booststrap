32013d0a6103a9bbf672fcc179b5f3b42d38ef93
guilherme <270210.gui@gmail.com>
2017-08-17 22:19:54 -0300

    "commenting banner affects"

--- bootstrap/js/navbar-animacao.js
+++ bootstrap/js/navbar-animacao.js
@@ -1,7 +1,8 @@
-$('#collapse-navbar').on('show.bs.collapse', function () {
+$('#collapse-navbar').on('show.bs.collapse', function () { //fazendo que o banner desça quando o menu for clicado para que o banner não fica encima do menu
     $('.topCasaFina-banner').css('transform', 'translate(-50%,10%)');
-});
+});                         
+                //ESSES EVENTOS ESTÁ NA DOCUMENTAÇÃO DO BOOTSTRAP DE EVENTOS COM COLLAPSE
 
-$('#collapse-navbar').on('hide.bs.collapse', function () {
+$('#collapse-navbar').on('hide.bs.collapse', function () {//fazendo o banner voltar a sua posição anterior quando o menu estiver oculto
     $('.topCasaFina-banner').css('transform', 'translate(-50%,-50%)');
 });
